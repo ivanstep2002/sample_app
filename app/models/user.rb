79ebad794fd8201763d66188056c11b888128893
ivanstep2002 <ivanstep2002@gmail.com>
2012-05-14 13:02:55 +0200

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,7 @@ class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
   has_secure_password
   before_save { |user| user.email = email.downcase }
+  before_save :create_remember_token
 
   validates :name, presence:TRUE, length:{maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -21,4 +22,8 @@ class User < ActiveRecord::Base
   validates :password, presence: true,length:{maximum: 6}
   validates :password_confirmation, presence: true
 
+  private
+  def create_remember_token
+    self.remember_token = SecureRandom.urlsafe_base64
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 SampleApp::Application.routes.draw do
   #get "users/new"
   resources :users
+  resources :sessions, only:[:new,:create,:destroy]
+
+
 
 root :to => 'static_pages#home'
  # get "static_pages/home"
@@ -12,6 +15,10 @@ root :to => 'static_pages#home'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
   match '/signup', to: 'users#new'
+
+  #match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
 
 
 
